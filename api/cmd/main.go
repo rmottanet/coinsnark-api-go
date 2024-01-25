3fa92a142f0c406d737648f39acd20f57290ba89
Rodrigo Motta <rmotta.net@gmail.com>
2024-01-25 15:51:51 -0300

    Coin Snark API

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,34 +1,74 @@
 package main
 
 import (
-	"fmt"
+	"os"
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
 	"github.com/rs/cors"
+	"github.com/gorilla/mux"
+
+	"coinsnark/api/pkg/cache"
+	"coinsnark/api/pkg/config"
+	"coinsnark/api/pkg/integrations"
+	"coinsnark/api/pkg/middleware"
+	"coinsnark/api/pkg/controllers"
+	"coinsnark/api/pkg/services"	
+	"coinsnark/api/pkg/html"
 )
 
 func main() {
-	// Carregar variáveis de ambiente
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Erro ao carregar arquivo .env")
+
+	config.LoadEnv()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
 	}
 
-	// Criar um roteador
-	router := mux.NewRouter()
-
-	// Configurar rotas
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome Coin Snark!")
+	corsHandler := cors.New(cors.Options{
+	    AllowedOrigins:   []string{"*"},
+	    AllowedMethods:   []string{"GET"},
+	    AllowedHeaders:   []string{"Content-Type"},
+	    AllowCredentials: true,
 	})
 
-	// Adicionar middleware CORS
-	handler := cors.Default().Handler(router)
+	exchangeRatesCache := cache.NewCurrencyCache()
+	
+	router := mux.NewRouter()
 
-	// Iniciar o servidor
-	fmt.Println("Servidor rodando na porta :8000...")
-	log.Fatal(http.ListenAndServe(":8000", handler))
+	router.Use(corsHandler.Handler)
+
+	currencyService := services.NewCurrencyService(exchangeRatesCache)
+	conversionService := services.NewConversionService(exchangeRatesCache)
+
+	currencyController := controllers.NewCurrencyController(currencyService)
+	conversionController := controllers.NewConversionController(conversionService)
+	
+
+	router.HandleFunc("/api/currency", currencyController.GetCurrencyNames).Methods("GET")
+
+    router.Handle("/api/convert", middleware.ValidateInput(middleware.ConvertHandler(conversionController.ConvertCurrency)))
+
+
+	if _, err := integrations.GetOpenExchangeRatesData(exchangeRatesCache); err != nil {
+		log.Fatalf("Error retrieving exchange rate data: %v", err)
+	}
+
+	if _, err := integrations.GetExchangeRatesData(exchangeRatesCache); err != nil {
+		log.Fatalf("Error retrieving exchange rate data: %v", err)
+	}
+	
+	
+    router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+        welcomeHTML := html.WelcomePageHTML()
+
+        w.Header().Set("Content-Type", "text/html")
+        w.WriteHeader(http.StatusOK)
+        w.Write([]byte(welcomeHTML))
+    })
+    
+	log.Printf("Server started on port %s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
