ef745fa872ee85e3d1daa4f477018ac95da34188
Rodrigo Motta <rmotta.net@gmail.com>
2024-01-25 15:46:31 -0300

    Currency Controller

---
+++ api/pkg/controllers/currency_controller.go
@@ -0,0 +1,47 @@
+package controllers
+
+import (
+    "time"
+    "encoding/json"
+    "net/http"
+
+    "coinsnark/api/pkg/models"
+    "coinsnark/api/pkg/services"
+)
+
+
+type CurrencyController struct {
+    Service services.CurrencyServiceInterface
+}
+
+
+func NewCurrencyController(service services.CurrencyServiceInterface) *CurrencyController {
+    return &CurrencyController{
+        Service: service,
+    }
+}
+
+// GetCurrencyNames retrieves currency symbols and names and returns them as a JSON response.
+func (currencyCtrl *CurrencyController) GetCurrencyNames(w http.ResponseWriter, r *http.Request) {
+
+    symbolsNames := currencyCtrl.Service.GetSymbolNames()
+
+    filteredCurrencies := make(map[string]string)
+    for symbol, name := range symbolsNames {
+        filteredCurrencies[symbol] = name
+    }
+
+    response := models.NewCurrencyResponse(filteredCurrencies, time.Now())
+
+    jsonResponse, err := json.Marshal(response)
+    if err != nil {
+        http.Error(w, "Error formatting response", http.StatusInternalServerError)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    
+    w.WriteHeader(http.StatusOK)
+    
+    w.Write(jsonResponse)
+}
