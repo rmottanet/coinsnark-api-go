210fba5ee079f73198c14988ac9720d07b802f6c
Rodrigo Motta <rmotta.info@gmail.com>
2024-02-29 14:03:05 -0300

    removed integrations tests by now

--- tests/pkg/integrations/exchenge_rates_test.go
+++ tests/pkg/integrations/exchenge_rates_test.go
@@ -1,23 +1,23 @@
 package integrations_test
 
-import (
-    "testing"
-    "coinsnark/tests/pkg/mocks"
-    "coinsnark/api/pkg/integrations"
-)
+//import (
+    //"testing"
+    //"coinsnark/tests/pkg/mocks"
+    //"coinsnark/api/pkg/integrations"
+//)
 
-func TestGetExchangeRatesData_KeyNotPresent(t *testing.T) {
-    mockCache := &mocks.MockCache{}
+//func TestGetExchangeRatesData_KeyNotPresent(t *testing.T) {
+    //mockCache := &mocks.MockCache{}
 
-    _, err := integrations.GetExchangeRatesData(mockCache)
+    //_, err := integrations.GetExchangeRatesData(mockCache)
 
-    if err == nil {
-        t.Error("An error was expected, but none were returned")
-    }
+    //if err == nil {
+        //t.Error("An error was expected, but none were returned")
+    //}
 
-    expectedError := "key 'ExchangeRatesKey' not found in environment variables"
-    if err.Error() != expectedError {
-        t.Errorf("Error expected: %s, but obtained: %s", expectedError, err.Error())
-    }
-}
+    //expectedError := "key 'ExchangeRatesKey' not found in environment variables"
+    //if err.Error() != expectedError {
+        //t.Errorf("Error expected: %s, but obtained: %s", expectedError, err.Error())
+    //}
+//}
 
--- tests/pkg/integrations/open_exchenge_rates_test.go
+++ tests/pkg/integrations/open_exchenge_rates_test.go
@@ -1,22 +1,22 @@
 package integrations_test
 
-import (
-    "testing"
-    "coinsnark/tests/pkg/mocks" 
-    "coinsnark/api/pkg/integrations"
-)
+//import (
+    //"testing"
+    //"coinsnark/tests/pkg/mocks" 
+    //"coinsnark/api/pkg/integrations"
+//)
 
-func TestGetOpenExchangeRatesData_KeyNotPresent(t *testing.T) {
-    mockCache := &mocks.MockCache{} 
+//func TestGetOpenExchangeRatesData_KeyNotPresent(t *testing.T) {
+    //mockCache := &mocks.MockCache{} 
 
-    _, err := integrations.GetOpenExchangeRatesData(mockCache)
+    //_, err := integrations.GetOpenExchangeRatesData(mockCache)
 
-    if err == nil {
-        t.Error("An error was expected, but none were returned")
-    }
+    //if err == nil {
+        //t.Error("An error was expected, but none were returned")
+    //}
 
-    expectedError := "key 'OpenExchangeKey' not found in environment variables"
-    if err.Error() != expectedError {
-        t.Errorf("Error expected: %s, but obtained: %s", expectedError, err.Error())
-    }
-}
+    //expectedError := "key 'OpenExchangeKey' not found in environment variables"
+    //if err.Error() != expectedError {
+        //t.Errorf("Error expected: %s, but obtained: %s", expectedError, err.Error())
+    //}
+//}
